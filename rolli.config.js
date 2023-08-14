74443c9a58a99d8654b0cb111600c965ed1bf02b
Ivo Dolenc <ivodolenc@gmail.com>
2023-08-14 14:12:14 +0200

    chore: update main `rolli` config

--- rolli.config.js
+++ rolli.config.js
@@ -2,6 +2,7 @@ import pkg from './package.json' assert { type: 'json' }
 
 export default {
   replace: {
+    preventAssignment: true,
     __name__: pkg.name.toUpperCase(),
     __version__: pkg.version,
   },
