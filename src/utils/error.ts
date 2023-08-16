8a1a9cae36cf54c3dfb38c9b2fc5beb0992fdfbd
Ivo Dolenc <ivodolenc@gmail.com>
2023-08-16 11:38:59 +0200

    refactor: update error util

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,10 +1,8 @@
-import { cl, ce, bold, red, darken } from 'colorate'
+import { cl, ce, bold, red } from 'colorate'
 import { name } from '../cli/meta.js'
 
 export const error = (err: any) => {
-  const time = new Date().toLocaleTimeString()
-
-  cl(bold(red(name)), `${darken('[' + time + ']')} Something went wrong...`)
+  cl(bold(red(name)), 'Something went wrong...')
   cl()
   ce(err)
   process.exit(1)
