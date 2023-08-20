2543d4886a6ebd50ce6950951b01a83f51541be8
Ivo Dolenc <ivodolenc@gmail.com>
2023-08-20 17:16:37 +0200

    chore: update eslint config

--- .config/eslint.config.js
+++ .config/eslint.config.js
@@ -7,10 +7,7 @@ export default [
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { ignoreRestSiblings: true },
-      ],
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ]
