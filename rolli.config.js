3500df49c0d049386a676bc9176061c598dcdf0a
Ivo Dolenc <ivodolenc@gmail.com>
2023-08-21 18:13:42 +0200

    chore: update main rolli config

--- rolli.config.js
+++ rolli.config.js
@@ -3,9 +3,11 @@ import pkg from './package.json' assert { type: 'json' }
 /** @type import('./src/types/options').RolliOptions */
 export default {
   tsconfig: 'node_modules/configshare/tsconfig.json',
-  replace: {
-    preventAssignment: true,
-    __name__: pkg.name.toUpperCase(),
-    __version__: pkg.version,
+  bin: {
+    replace: {
+      preventAssignment: true,
+      __name__: pkg.name.toUpperCase(),
+      __version__: pkg.version,
+    },
   },
 }
