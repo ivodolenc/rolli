c9e03e76953544b5797f8841946d6c87a39688f8
Ivo Dolenc <ivodolenc@gmail.com>
2023-08-28 20:18:09 +0200

    feat: add matcher option

--- src/utils/get-input-path.ts
+++ src/utils/get-input-path.ts
@@ -1,11 +1,26 @@
 import { parse } from 'node:path'
 import { exists } from 'utills/node'
 
-export async function getInputPath(srcDir: string, output: string) {
+export async function getInputPath(
+  srcDir: string,
+  output: string,
+  matcher?: string,
+) {
   const outputDir = output.split('/')[1]
-  const inputDir = output.replace(outputDir, srcDir)
-  const inputJs = inputDir.replace(parse(inputDir).ext, '.js')
-  const inputTs = inputDir.replace(parse(inputDir).ext, '.ts')
+  const outputPath = output.replace(outputDir, srcDir)
+  let inputJs = ''
+  let inputTs = ''
+
+  if (!matcher) {
+    const outputExt = parse(outputPath).ext
+    inputJs = outputPath.replace(outputExt, '.js')
+    inputTs = outputPath.replace(outputExt, '.ts')
+  } else {
+    const outputBase = parse(outputPath).base
+    inputJs = outputPath.replace(outputBase, `${matcher}.js`)
+    inputTs = outputPath.replace(outputBase, `${matcher}.ts`)
+  }
+
   const fileJs = await exists(inputJs)
 
   return fileJs ? inputJs : inputTs
