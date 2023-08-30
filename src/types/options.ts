1c4b8899e8cb6ba95beac402f23a10b59ff700f3
Ivo Dolenc <ivodolenc@gmail.com>
2023-08-30 16:47:12 +0200

    feat: extend plugins option

--- src/types/options.ts
+++ src/types/options.ts
@@ -7,18 +7,29 @@ import type { Options as DtsOptions } from 'rollup-plugin-dts'
 
 type Externals = (string | RegExp)[]
 
-type ExportsExclude = {
+interface ExportsExclude {
   path: string
   types?: true
   import?: true
   require?: true
 }
 
-type ExportsMatcher = {
+interface ExportsMatcher {
   default?: string
   types?: string
   import?: string
   require?: string
+}
+
+interface PluginsOptions {
+  /**
+   * Runs `before` the default plugins.
+   */
+  start: Plugin[]
+  /**
+   * Runs `after` the default plugins.
+   */
+  end: Plugin[]
 }
 
 export interface Plugins {
@@ -37,7 +48,7 @@ export interface ExportsOptions extends Plugins {
   tsconfig?: string
   exclude?: (string | ExportsExclude)[]
   matcher?: ExportsMatcher
-  plugins?: Plugin[]
+  plugins?: Plugin[] | PluginsOptions
 }
 
 export interface BinOptions extends Omit<Plugins, 'dts'> {
@@ -48,7 +59,7 @@ export interface BinOptions extends Omit<Plugins, 'dts'> {
   tsconfig?: string
   exclude?: string[]
   matcher?: string
-  plugins?: Plugin[]
+  plugins?: Plugin[] | PluginsOptions
 }
 
 export interface EntriesOptions extends Plugins {
@@ -61,7 +72,7 @@ export interface EntriesOptions extends Plugins {
   footer?: OutputOptions['footer']
   minify?: boolean
   tsconfig?: string
-  plugins?: Plugin[]
+  plugins?: Plugin[] | PluginsOptions
 }
 
 export interface RolliOptions {
