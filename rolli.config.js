a06c33ea9694d3c4c0e9772884ba0fd2a5245fce
Ivo Dolenc <ivodolenc@gmail.com>
2023-09-20 15:43:35 +0200

    chore: update rolli config

--- rolli.config.js
+++ rolli.config.js
@@ -2,7 +2,6 @@ import pkg from './package.json' assert { type: 'json' }
 
 /** @type {import('./src/types/options').RolliOptions} */
 export default {
-  tsconfig: 'node_modules/configshare/tsconfig.json',
   bin: {
     externals: [/node:/, /rollup/, /utills/, ...Object.keys(pkg.dependencies)],
     replace: {
